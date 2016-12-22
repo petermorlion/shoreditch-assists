eb4b1dba0384c7d4f3eda4887e29ea08022c298b
Peter Morlion <peter.morlion@gmail.com>
2016-12-22 21:32:47 +0100

    Don't reserve space for featured image on column page if there isn't one

--- src/template-parts/content-column-page.php
+++ src/template-parts/content-column-page.php
@@ -12,7 +12,9 @@
 <article class="column-page-column" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="hentry-wrapper">
+		<?php if (has_post_thumbnail()) { ?>
 		<div class="column-page-column-img" <?php shoreditch_background_image('thumbnail'); ?>></div>
+		<?php } ?>
 		<header class="entry-header">
 			<?php
 				the_title( '<h3 class="entry-title">', '</h3>' );
